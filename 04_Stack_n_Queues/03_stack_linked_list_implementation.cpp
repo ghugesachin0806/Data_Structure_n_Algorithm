8e983bf87bbd19d52851cc1e6278ab4a72755138
sachin_ghuge <ghugesachin0806@gmail.com>
2022-12-21 10:35:12 +0530

    first

---
+++ 04_Stack_n_Queues/03_stack_linked_list_implementation.cpp
@@ -0,0 +1,147 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    // constructor
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+
+    // destructor
+    ~Node()
+    {
+        int value = this->data;
+        if (this->next != NULL)
+        {
+            delete this->next;
+            this->next = NULL;
+        }
+        cout << "Memory is free for " << value << endl;
+    }
+};
+
+class Stack
+{
+private:
+    Node *top;
+    int cnt;
+
+public:
+    // constructor
+    Stack()
+    {
+        this->top = NULL;
+        this->cnt = 0;
+    }
+
+    // Push function
+    void push(int data)
+    {
+        // Create new node temp and allocate memory in heap
+        Node *temp = new Node(data);
+
+        // Check if stack (heap) is full.
+        // Then inserting an element would
+        // lead to stack overflow
+
+        if (!temp) // means if(! NULL)
+        {
+            cout << "stack over flow" << endl;
+
+            exit(1); // Abnormal termination of the program -> exit(1)
+                     // Succesful termination of the program -> exit(0)
+        }
+
+        temp->next = top;
+        top = temp;
+        this->cnt++;
+    }
+
+    // pop function
+    void pop()
+    {
+        if (top == NULL)
+        {
+            cout << "Stack is empty !" << endl;
+            exit(1);
+        }
+
+        Node *temp = top;
+        top = top->next;
+        temp->next = NULL;
+        delete temp;
+        cnt--;
+    }
+
+    // empty function
+    bool empty()
+    {
+        return top == NULL;
+    }
+
+    // top element
+    int top_ele()
+    {
+        if (top == NULL)
+        {
+            cout << "Stack is empty !" << endl;
+            return -1;
+        }
+
+        return top->data;
+    }
+
+    // size function
+    int size()
+    {
+        return cnt;
+    }
+
+    // display all the elements
+    void display()
+    {
+        if (top != NULL)
+        {
+            Node *temp;
+            temp = top;
+            cout << "stack elements are : ";
+            while (temp != NULL)
+            {
+                cout << temp->data << " ";
+                temp = temp->next;
+            }
+        }
+        else
+        {
+            cout << "stack is empty !" << endl;
+        }
+    }
+};
+
+int main()
+{
+    Stack st;
+
+    st.push(8);
+    st.push(18);
+    st.push(78);
+
+    cout << "top element : " << st.top_ele() << endl;
+    cout << "size of stack : " << st.size() << endl;
+
+    st.pop();
+
+    cout << "top element : " << st.top_ele() << endl;
+    cout << "size of stack : " << st.size() << endl;
+
+    st.display();
+
+    return 0;
+}
