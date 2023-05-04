5b9e6d6c995a52b6ae9e3b1832285b914aad7c02
sachin_ghuge <ghugesachin0806@gmail.com>
2023-05-04 19:00:49 +0530

    first

---
+++ 13_Dynamic_programming/24_D_Number_of_subsets(subsequences)_with_sum_k_Recursion_(special_case).cpp
@@ -0,0 +1,36 @@
+// https://www.codingninjas.com/codestudio/problems/number-of-subsets_3952532
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int solve(int index, int target, vector<int> &num)
+{
+    if (index == 0)
+    {
+        if (target == 0 && num[index] == 0)
+            return 2;
+        if (target == 0 || target == num[index])
+            return 1;
+        else
+            return 0;
+    }
+
+    // Exclude call
+    int excl = solve(index - 1, target, num);
+
+    // Include call
+    int incl = 0;
+    if (target - num[index] >= 0)
+        incl = solve(index - 1, target - num[index], num);
+
+    return incl + excl;
+}
+
+int findWays(vector<int> &num, int tar)
+{
+    // Write your code here.
+
+    int curr_index = num.size() - 1;
+
+    return solve(curr_index, tar, num);
+}
